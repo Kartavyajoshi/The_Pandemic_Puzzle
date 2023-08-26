75241cb31f0cb3f73bf7bc9f8d870dc9d7300e56
Dikshen Makwana <makwanadikshen@gmail.com>
2023-08-26 12:14:04 +0530

    added links to other pages

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <title>Pandemic Puzzle</title>
     <link rel="stylesheet" href="index.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="shortcut icon" href="healthcare.png" type="image/x-icon">
@@ -15,36 +15,6 @@
     />
   </head>
   <body>
-    
-    <!-- <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#"><img src="logor.png" width="80px" alt=""></a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-      
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav">
-                <li class="nav-item"><a  class="nav-link active" href="#">About</a></li>
-                <li class="nav-item"><a  class="nav-link active" href="#">Donate</a></li>
-                <li class="nav-item"><a  class="nav-link active" href="#">Book your Slot</a></li>
-                <li class="nav-item"><a  class="nav-link active" href="#">Check your health</a></li>
-                <li class="nav-item"><a  class="nav-link active" href="#">Contact doctor</a></li>
-                <li class="nav-item"><a  class="nav-link active" href="#">Support</a></li>
-
-            </ul>
-            
-            <form class="d-flex " id="sbox" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-              </form>
-          
-            </div>
-        
-        </div>
-      </nav> -->
-
-    
     <div>
         <div class=" text-center margin marginbase">
             <img class="d-block mx-auto" src="healthcare.png" alt="" width="72" height="65">
@@ -52,8 +22,8 @@
             <div class="col-lg-6 mx-auto">
               <p class="lead mb-4">Welcome to our health care website, where we are committed to providing you with the highest quality care and services. Our team of experienced and dedicated healthcare professionals is here to help you achieve your health goals and improve your overall well-being.</p>
               <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Log in</button>
-                <button type="button" class="btn btn-outline-secondary btn-lg px-4">Sign up</button>
+                <button type="button" class="btn btn-primary btn-lg px-4 gap-3"><a href="Login.php" style="text-decoration:none; color:white" > Log in</a></button>
+                <button type="button" class="btn btn-outline-secondary btn-lg px-4"><a href="SignUp.php" style="text-decoration:none; color:black"> Sign up</a></button>
               </div>
             </div>
           </div>
@@ -62,7 +32,7 @@
     <div>
     <center>
        <footer>
-        <p >© Copyright 2023   E-healthcare   Inc. All right reserved</p>
+        <p>© Copyright | <?php echo date("Y"); ?>   E-healthcare   Inc. All right reserved</p>
        </footer>
     </center>
     </div>
